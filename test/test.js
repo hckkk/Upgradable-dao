11bb6957102c4c009f1ab9a4bf1b3f6a8a04f015
hckkk <2659871636@qq.com>
2023-10-23 18:14:29 -0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,9 +4,14 @@ const { expect } = require("chai");
 describe("Dao", function () {
   it("shoud deploy and store 1 eth and with draw one eth", async function () {
     const [owner] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", owner.address);
 
     const Dao = await ethers.deployContract("Dao");
-    console.log("Dao deployed to:", Dao.address);
+    console.log("Dao address:", await Dao.getAddress());
+
+    const DaoBalance = await Dao.getBalance();
+    console.log("Dao balance:", DaoBalance.toString());
+
 
   });
 });
